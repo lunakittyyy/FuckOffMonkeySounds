69bcbd2efb63442544b2fc7a2b0590300863013c
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-03-02 15:14:54 -0500

    Initial commit

---
+++ FuckOffMonkeySounds/FOMSMod.cs
@@ -0,0 +1,19 @@
+﻿using FuckOffMonkeySounds;
+using HarmonyLib;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(FOMSMod), "FuckOffMonkeySounds", "0.2.0", "Luna")]
+[assembly: MelonGame("Another Axiom", "Gorilla Tag")]
+namespace FuckOffMonkeySounds
+{
+    public class FOMSMod : MelonMod { }
+
+    [HarmonyPatch(typeof(VRRig), "LateUpdate")]
+    static class ComputerPatch
+    {
+        static void Prefix(VRRig __instance)
+        {
+            __instance.replacementVoiceLoudnessThreshold = float.MaxValue;
+        }
+    }
+}
