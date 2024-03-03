d391f53a6e919d53c9cd6e7e6cf3dae38b3fd41c
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-03-03 10:28:04 -0500

    oops

--- FuckOffMonkeySounds/FOMSMod.cs
+++ FuckOffMonkeySounds/FOMSMod.cs
@@ -1,13 +1,31 @@
-﻿using FuckOffMonkeySounds;
-using HarmonyLib;
+﻿using HarmonyLib;
+#if MELONLOADER
 using MelonLoader;
+using FuckOffMonkeySounds;
+#else
+using BepInEx;
+using System.Reflection;
+#endif
 
-[assembly: MelonInfo(typeof(FOMSMod), "FuckOffMonkeySounds", "1.0.0", "Luna")]
+#if MELONLOADER
+[assembly: MelonInfo(typeof(FOMSMod), "FuckOffMonkeySounds", "1.1.0", "Luna")]
 [assembly: MelonGame("Another Axiom", "Gorilla Tag")]
+#endif
 namespace FuckOffMonkeySounds
 {
+#if MELONLOADER
     public class FOMSMod : MelonMod { }
-
+#else
+    [BepInPlugin("luna.fuckoffmonkeysounds", "FuckOffMonkeySounds", "1.1.0")]
+    public class FOMSMod : BaseUnityPlugin {
+        
+        void Start()
+        {
+            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("luna.fuckoffmonkeysounds");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+#endif
     [HarmonyPatch(typeof(VRRig), "LateUpdate")]
     static class RigPatch
     {
